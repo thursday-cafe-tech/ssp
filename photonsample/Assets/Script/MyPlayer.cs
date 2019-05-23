2769416c5002e0b03732b4449a3d03517f0f04a9
ishiyama <darosu@infoseek.jp>
2019-05-24 04:19:04 +0900

    R vert "gitignoreを適応するためにディレクトリ構造を変更"
    This reverts commit 879d2c4e76864d6652631f83b15a2ab5fe85ed02.

---
+++ photonsample/Assets/Script/MyPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class MyPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Text>().text = PhotonNetwork.LocalPlayer.ActorNumber.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("PlayerCount: " + PhotonNetwork.CurrentRoom.PlayerCount);
+        Debug.Log("CurrntRoom: " + PhotonNetwork.CurrentRoom);
+        Debug.Log("PlayerListLength: " + PhotonNetwork.PlayerList.Length);
+        foreach (Player p in PhotonNetwork.PlayerList){
+            Debug.Log("Player: " + p);
+            Debug.Log("ActorNumber: " + p.ActorNumber);
+        }
+    }
+}
