b542eabd0bf6f247cb6ff8902b28a1bb5a9223f1
yuukieyuukie <toprankedcloser@gmail.com>
2019-05-25 13:42:47 +0900

    新規作成

---
+++ photonsample/Assets/Script/MyPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class MyPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Text>().text = PhotonNetwork.LocalPlayer.ActorNumber.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
